3f17b5043dafb87acda3b2d21c39399f870d40c3
jmelnick101 <148572165+jmelnick101@users.noreply.github.com>
2025-04-08 12:53:43 -0500

    Update makeTanglegram.R

--- makeTanglegram.R
+++ makeTanglegram.R
@@ -1,13 +1,15 @@
 library(ape)
 
-#gene tree
-nifh <- read.tree("nifHphylo/RAxML_bestTree.nifHphylogeny.tre")
+#gene trees
+nifH <- read.tree("nifHphylo/RAxML_bestTree.nifHphylogeny.tre")
+nodA <- read.tree("nodAphylo/RAxML_bestTree.nodAphylogeny.tre")
+nodB <- read.tree("nodBphylo/RAxML_bestTree.nodBphylogeny.tre")
 
 #wgs phylogeny
-wgs <- read.tree("JoshuaPhylophlan/allmesogenomes_phylophlan/RAxML_bestTree.allmesogenomes_refined_short.tre")
+wgs <- read.tree("JoshuaPhylophlan/allmesogenomes_phylophlan/RAxML_bestTree.allmesogenomes_refined.tre")
 
 #link the matching labels with a line; both trees should have the same labels
-association <- cbind(wgs$tip.label, wgs$tip.label)
+association <- cbind(nodA$tip.label, nodA$tip.label)
 
 #make tanglegram
-cophyloplot(wgs,nifh,association, gap=5,length.line=0,space=100)
+cophyloplot(nodA,nodB,association, gap=10,length.line=0,space=100,rotate=TRUE)
