dbb4f8492cc835f7a7adec6d8bf962d399dcb11b
jmelnick101 <148572165+jmelnick101@users.noreply.github.com>
2025-04-11 13:47:52 -0500

    Update makeMantel.R
    Changed some file names, actual code is the same

--- makeMantel.R
+++ makeMantel.R
@@ -3,10 +3,10 @@ library(ape)
 library(ade4)
 
 #import geographic distance matrix
-geodist <- as.dist(read.table("geodistmatupdate3.txt"),diag=TRUE)
+geodist <- as.dist(read.table("GeoDistMatFinal.txt"),diag=TRUE)
 
 #import genetic distance matrix from alignment
-phylodist <- as.dist(dist.alignment(read.alignment("JoshuaPhylophlan/mesorhizobia_metagenomes_phylophlan/mesorhizobia_metagenomes_concatenated2.aln",format="fasta")),diag=TRUE)
+phylodist <- as.dist(dist.alignment(read.alignment("JoshuaPhylophlan/allmesogenomes_phylophlan/allmesogenomes_concatenated.aln",format="fasta")),diag=TRUE)
 
 #ade4
 mantel.rtest(geodist,phylodist)
