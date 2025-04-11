b1603a0b811fdac4c8feeb359a837280938b5eda
jmelnick101 <148572165+jmelnick101@users.noreply.github.com>
2025-04-11 11:43:34 -0500

    Update getNucGenesFromProkka.py
    Fixed code so it actually works. Also prints out any annotations the genes might be missing from.

--- getNucGenesFromProkka.py
+++ getNucGenesFromProkka.py
@@ -2,17 +2,22 @@ from Bio import SeqIO
 import os
 
 #replace with any gene you need
-gene = "recA"
+gene = "RecA"
+#replace with whatever you named your folder of Prokka results
+foldpath = "finalrun"
 
 #iterate through every folder produced by Prokka (one per genome) in a directory I called finalrun
-for annotation in os.listdir("finalrun"):
+for annotation in os.listdir("{}".format(foldpath)):
+    found = False
     #search through each folder's ffn file - this is a list of gene names with their nucleotides; for amino acids, replace ffn with faa
-    recs = SeqIO.parse("{0}/{0}.ffn".format(annotation), "fasta")
+    recs = SeqIO.parse("{0}/{1}/{1}.ffn".format(foldpath,annotation), "fasta")
     #loop through every gene
     for record in recs:
         #genes have unnecessary identifiers between the > and the actual gene name, so just check if it's in the line rather than the full name
-        if gene in record.id:
+        if gene in record.description:
+            found = True
             with open("metagenome_{}.fasta".format(gene), "a") as g:
                 #write to a fasta file
                 g.write(">"+annotation+"\n"+str(record.seq)+"\n")
-
+    if found == False:
+        print("{0} is missing from {1}".format(gene,annotation))
