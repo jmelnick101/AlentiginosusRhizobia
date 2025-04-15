de7f316f222d6286095169a4ac77c7f1449faeb2
jmelnick101 <148572165+jmelnick101@users.noreply.github.com>
2025-04-15 16:40:05 -0500

    Update makeTanglegram.R

--- makeTanglegram.R
+++ makeTanglegram.R
@@ -1,15 +1,20 @@
 library(ape)
 
 #gene trees
-nifH <- read.tree("nifHphylo/RAxML_bestTree.nifHphylogeny.tre")
-nodA <- read.tree("nodAphylo/RAxML_bestTree.nodAphylogeny.tre")
-nodB <- read.tree("nodBphylo/RAxML_bestTree.nodBphylogeny.tre")
+#nifH <- read.tree("nifHphylo/RAxML_bestTree.nifHphylogeny.tre")
+#nodA <- read.tree("nodAphylo/RAxML_bestTree.nodAphylogeny.tre")
+#nodB <- read.tree("nodBphylo/RAxML_bestTree.nodBphylogeny.tre")
+
+#concatenated nifH, nodA, and nodB trees
+con <- read.tree("iqtest/genealignments.treefile")
+#housekeeping gene
+recA <- read.tree("recAphylo/RAxML_bestTree.recAphylogeny.tre")
 
 #wgs phylogeny
-wgs <- read.tree("JoshuaPhylophlan/allmesogenomes_phylophlan/RAxML_bestTree.allmesogenomes_refined.tre")
+#wgs <- read.tree("JoshuaPhylophlan/allmesogenomes_phylophlan/RAxML_bestTree.allmesogenomes_refined.tre")
 
 #link the matching labels with a line; both trees should have the same labels
-association <- cbind(nodA$tip.label, nodA$tip.label)
+association <- cbind(con$tip.label, con$tip.label)
 
 #make tanglegram
-cophyloplot(nodA,nodB,association, gap=10,length.line=0,space=100,rotate=TRUE)
+cophyloplot(con,recA,association, gap=10,length.line=0,space=100,rotate=TRUE)
