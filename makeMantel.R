de93e3bc9314185998ee981c5f9bb065b8ffedb1
jmelnick101 <148572165+jmelnick101@users.noreply.github.com>
2025-04-21 16:15:01 -0500

    Update makeMantel.R

--- makeMantel.R
+++ makeMantel.R
@@ -1,6 +1,7 @@
 library(seqinr)
-library(ape)
-library(ade4)
+library(vegan)
+#library(ade4)
+#library(ape)
 
 #import geographic distance matrix
 geodist <- as.dist(read.table("GeoDistMatFinal.txt"),diag=TRUE)
@@ -8,12 +9,23 @@ geodist <- as.dist(read.table("GeoDistMatFinal.txt"),diag=TRUE)
 #import genetic distance matrix from alignment
 phylodist <- as.dist(dist.alignment(read.alignment("JoshuaPhylophlan/allmesogenomes_phylophlan/allmesogenomes_concatenated.aln",format="fasta")),diag=TRUE)
 
+#if samples are the same variety, they have distance of 0; if they are different varieties, they have distance of 1
 variety <- as.dist(read.table("varietymatrix",check.names=FALSE),diag=TRUE)
 
-#ade4
-mantel.rtest(phylodist,geodist) #genetic distance by geographic distance
+#vegan
+mantel(phylodist,geodist) #genetic distance by geographic distance
+mantel.partial(phylodist,geodist,variety) #genetic distance by geographic distance, controlling for variety
 
-mantel.rtest(phylodist,variety) #genetic distance by variety
+mantel(phylodist,variety) #genetic distance by variety
+mantel.partial(phylodist,variety,geodist) #genetic distance by variety, controlling for geographic distance
+
+#mantel(geodist,variety) #geographic distance by variety (not the focus of this study)
+#mantel.partial(geodist,variety,phylodist) #geographic distance by variety, controlling for genetic distance
+
+
+#ade4
+#mantel.rtest(phylodist,geodist) #genetic distance by geographic distance
+#mantel.rtest(phylodist,variety) #genetic distance by variety
 
 #ape
 #mantel.test(as.matrix(geodist),as.matrix(phylodist))
