a5304ad8e127fab0a337256c5d54bf193a816af3
jmelnick101 <148572165+jmelnick101@users.noreply.github.com>
2025-04-21 14:48:46 -0500

    Update makeMantel.R

--- makeMantel.R
+++ makeMantel.R
@@ -8,8 +8,12 @@ geodist <- as.dist(read.table("GeoDistMatFinal.txt"),diag=TRUE)
 #import genetic distance matrix from alignment
 phylodist <- as.dist(dist.alignment(read.alignment("JoshuaPhylophlan/allmesogenomes_phylophlan/allmesogenomes_concatenated.aln",format="fasta")),diag=TRUE)
 
+variety <- as.dist(read.table("varietymatrix",check.names=FALSE),diag=TRUE)
+
 #ade4
-mantel.rtest(geodist,phylodist)
+mantel.rtest(phylodist,geodist) #genetic distance by geographic distance
+
+mantel.rtest(phylodist,variety) #genetic distance by variety
 
 #ape
-mantel.test(as.matrix(geodist),as.matrix(phylodist))
+#mantel.test(as.matrix(geodist),as.matrix(phylodist))
