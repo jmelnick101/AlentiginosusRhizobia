21a2eb17170b5380f99b3b7f300f64de9a4a0a76
jmelnick101 <148572165+jmelnick101@users.noreply.github.com>
2025-05-08 11:03:37 -0500

    Add files via upload
    Change geographic site order to match genetic site order

---
+++ reorder.py
@@ -0,0 +1,21 @@
+#reorder sites based on order of alignment/tree so that matrices match
+
+#read genetic order
+with open("order.txt", "r") as f:
+    names = f.readlines()
+#strip newlines
+for n in range(len(names)):
+    names[n] = names[n].strip()
+
+#get list of sites in the old order with their coordinates
+with open("sitesFinal.txt", "r") as s:
+    old = s.readlines()
+
+#create new file with the sites reordered to match the genetic order
+with open("newOrder.txt", "a") as x:
+    #iterate through genetic order
+    for name in names:
+        #iterate through old site order until you find the one you are looking for
+        for site in old:
+            if site.split('\t')[0] == name:
+                x.write(site)
