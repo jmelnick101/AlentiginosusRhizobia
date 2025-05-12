6393d041ce8c33ebbdba683c2e3fcd6d1396ac93
jmelnick101 <148572165+jmelnick101@users.noreply.github.com>
2025-05-12 12:22:09 -0500

    Update makeTanglegram.R
    Use MEGA instead of IQTree

--- makeTanglegram.R
+++ makeTanglegram.R
@@ -6,15 +6,17 @@ library(ape)
 #nodB <- read.tree("nodBphylo/RAxML_bestTree.nodBphylogeny.tre")
 
 #concatenated nifH, nodA, and nodB trees
-con <- read.tree("iqtest/genealignments.treefile")
+#con <- read.tree("genealignments/iqtestfinal/genealignments.treefile")
+con <- read.tree("concatPhylo/RAxML_bipartitions.concatPhylogeny.tre")
 #housekeeping gene
-recA <- read.tree("recAphylo/RAxML_bestTree.recAphylogeny.tre")
+#recA <- read.tree("recAphylo/RAxML_bestTree.RecAphylogeny.tre")
 
 #wgs phylogeny
 #wgs <- read.tree("JoshuaPhylophlan/allmesogenomes_phylophlan/RAxML_bestTree.allmesogenomes_refined.tre")
+wgs <- read.tree("JoshuaPhylophlan/genomesnobins_phylophlan/boot/RAxML_bipartitions.genomesnobins_boot.tre")
 
 #link the matching labels with a line; both trees should have the same labels
 association <- cbind(con$tip.label, con$tip.label)
 
 #make tanglegram
-cophyloplot(con,recA,association, gap=10,length.line=0,space=100,rotate=TRUE)
+cophyloplot(con,wgs,association, gap=10,length.line=0,space=100,rotate=TRUE)
