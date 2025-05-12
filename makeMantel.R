285cb1798f2685caba4772ddd8a74db78c338ed6
jmelnick101 <148572165+jmelnick101@users.noreply.github.com>
2025-05-12 12:37:29 -0500

    Update makeMantel.R
    Added versions for symbiosis genes and shortened set in the comments. Probably not too useful though since anyone else will have the files named differently anyway.

--- makeMantel.R
+++ makeMantel.R
@@ -4,13 +4,19 @@ library(vegan)
 #library(ape)
 
 #import geographic distance matrix
-geodist <- as.dist(read.table("GeoDistMatFinal.txt"),diag=TRUE)
+geodist <- as.dist(read.table("GeoDistMatFinal.txt"),diag=TRUE) #full 104 genomes
+#geodist <- as.dist(read.table("symGeoDistMat.txt"),diag=TRUE) #concatenated symbiosis genes from 77 genomes with strains and multiple bins from the same metagenome removed
+#geodist <- as.dist(read.table("shortGeoDistMat.txt"),diag=TRUE) #full genomes from 77 genomes with strains and multiple bins from the same metagenome removed
 
 #import genetic distance matrix from alignment
-phylodist <- as.dist(dist.alignment(read.alignment("JoshuaPhylophlan/allmesogenomes_phylophlan/allmesogenomes_concatenated.aln",format="fasta")),diag=TRUE)
+phylodist <- as.dist(dist.alignment(read.alignment("JoshuaPhylophlan/allmesogenomes_phylophlan/allmesogenomes_concatenated.aln",format="fasta")),diag=TRUE) #full 104 genomes
+#phylodist <- as.dist(dist.alignment(read.alignment("Concatenated_alignments.fas",format="fasta")),diag=TRUE) #concatenated symbiosis genes from 77 genomes with strains and multiple bins from the same metagenome removed
+#phylodist <- as.dist(dist.alignment(read.alignment("JoshuaPhylophlan/genomesnobins_phylophlan/genomesnobins_concatenated.aln",format="fasta")),diag=TRUE) #full genomes from 77 genomes with strains and multiple bins from the same metagenome removed
 
 #if samples are the same variety, they have distance of 0; if they are different varieties, they have distance of 1
-variety <- as.dist(read.table("varietymatrix",check.names=FALSE),diag=TRUE)
+variety <- as.dist(read.table("varietymatrix",check.names=FALSE),diag=TRUE) #full 104 genomes
+#variety <- as.dist(read.table("symvarietymatrix",check.names=FALSE),diag=TRUE) #concatenated symbiosis genes from 77 genomes with strains and multiple bins from the same metagenome removed
+#variety <- as.dist(read.table("shortvarietymatrix",check.names=FALSE),diag=TRUE) #full genomes from 77 genomes with strains and multiple bins from the same metagenome removed
 
 #vegan
 mantel(phylodist,geodist) #genetic distance by geographic distance
