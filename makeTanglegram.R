1ca3ceb4dd470d767b65f06d35a96073a49ef604
jmelnick101 <148572165+jmelnick101@users.noreply.github.com>
2025-05-23 11:07:21 -0500

    Update makeTanglegram.R
    Added tanglegrams for just Mesorhizobium or just Allomesorhizobium. You would need those tree files though, so I'm just commenting it.

--- makeTanglegram.R
+++ makeTanglegram.R
@@ -15,8 +15,19 @@ con <- read.tree("concatPhylo/RAxML_bipartitions.concatPhylogeny.tre")
 #wgs <- read.tree("JoshuaPhylophlan/allmesogenomes_phylophlan/RAxML_bestTree.allmesogenomes_refined.tre")
 wgs <- read.tree("JoshuaPhylophlan/genomesnobins_phylophlan/boot/RAxML_bipartitions.genomesnobins_boot.tre")
 
+#just mesorhizobium trees
+#mesowgs <- read.tree("JoshuaPhylophlan/justmeso_phylophlan/boot/RAxML_bipartitions.justmeso_boot.tre")
+#mesosym <- read.tree("justmesosymphylo/RAxML_bipartitions.justmesosym_boot.tre")
+#just allomesorhizobium trees
+#allowgs <- read.tree("JoshuaPhylophlan/justallo_phylophlan/boot/RAxML_bipartitions.justallo_boot.tre")
+#allosym <- read.tree("justallosymphylo/RAxML_bipartitions.justallosym_boot.tre")
+
 #link the matching labels with a line; both trees should have the same labels
 association <- cbind(con$tip.label, con$tip.label)
+#association <- cbind(mesowgs$tip.label, mesowgs$tip.label)
+#association <- cbind(allowgs$tip.label, allowgs$tip.label)
 
 #make tanglegram
 cophyloplot(con,wgs,association, gap=10,length.line=0,space=100,rotate=TRUE)
+#cophyloplot(mesosym,mesowgs,association, gap=10,length.line=0,space=100,rotate=TRUE)
+#cophyloplot(allosym,allowgs,association, gap=10,length.line=0,space=100,rotate=TRUE)
